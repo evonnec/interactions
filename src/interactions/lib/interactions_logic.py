7dfc223ead224e082684617e8d65a2faa10ba0dd
Evonne Cho <evonne.cho@gmail.com>
2020-09-14 13:33:29 -0400

    Working loading data from json

--- src/interactions/lib/interactions_logic.py
+++ src/interactions/lib/interactions_logic.py
@@ -1,4 +1,15 @@
 
+import json
+
+def load_interactions_from_list(json_list):
+    result = []
+    for item in json_list:
+        drugs = item['drugs']
+        severity = item['severity']
+        description = item['description']
+        new_interaction = Interaction(drugs=drugs, severity=severity, description=description)
+        result.append(new_interaction)
+    return result
 
 def find_interactions(interactions, drugs):
     result = []
@@ -7,8 +18,27 @@ def find_interactions(interactions, drugs):
             result.append(interaction)
     return result
 
+def get_interactions(drugs):
+    with open('interactions.json') as f:
+        json_list = json.load(f)
+    interactions = load_interactions_from_list(json_list)   
+    result = find_interactions(interactions=interactions, drugs=drugs) 
+    return result
+
+
 class Interaction:
     def __init__(self, drugs, severity, description):
         self.drugs = drugs
         self.severity = severity
         self.description = description
+
+    def __eq__(self, other):
+        if self.drugs == other.drugs and self.severity == other.severity and self.description == other.description:
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        
+        return '<interaction: ' + str(self.drugs) + '>'
+
