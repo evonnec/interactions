6d257a728ce3b1fb641a2de4a4763562235f7080
Evonne Cho <evonne.cho@gmail.com>
2023-07-17 20:20:19 -0400

    add data, add making input file, add .gitignore

---
+++ src/interactions/make_input_file.py
@@ -0,0 +1,5 @@
+
+string_of_drugs = 'sildenafil tamsulosin nitroglycerin nicotine lovastatin enflurane rescinnamine enalaprilat nicardipine echinacea norethisterone meloxicam thiamylal benzylpenicillin guanoxan grepafloxacin lofexidine guanoclor norgestimate cyclopenthiazide'
+
+for i in range(1000):
+    print(string_of_drugs)
